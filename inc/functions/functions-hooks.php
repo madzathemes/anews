093022f176ef2bb0e8b79daf0e3a53376e2a82b5
madzathemes <madza@madzathemes.com>
2017-10-03 18:12:31 +0300

    fix

--- inc/functions/functions-hooks.php
+++ inc/functions/functions-hooks.php
@@ -377,6 +377,7 @@ add_action( 'wp_enqueue_scripts', 'anews_css');
 function anews_header_script() {
 
 		$option = get_option("anews_theme_options");
+		wp_enqueue_script( 'mt-defer', get_template_directory_uri(). '/inc/js/defer.js', array( 'jquery'),  '1.0', true );
 
 		wp_enqueue_script( 'anews_script', get_template_directory_uri(). '/inc/js/scripts.js', array( 'jquery'), '', true );
 		wp_localize_script( 'anews_script', 'ajax_posts', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ), 'noposts' => esc_html__('No older posts found', 'anews'), ));
